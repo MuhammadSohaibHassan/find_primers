c3379ede14896db0f182f856bbf90213ff09fb04
MuhammadSohaibHassan <124514984+MuhammadSohaibHassan@users.noreply.github.com>
2025-01-22 00:22:31 +0500

    Update get-dir-length.ps1

--- get-dir-length.ps1
+++ get-dir-length.ps1
@@ -1,8 +1,1 @@
-# Get the current working directory
-$currentDir = Get-Location
-
-# Get the length of the current directory path
-$length = $currentDir.Path.Length
-
-# Print the length
-Write-Host "The length of the current working directory is: $length characters"
+Write-Host "Current working directory length: $((Get-Location).Path.Length)" -ForegroundColor Green
